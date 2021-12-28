30d50170720a8ee765ca2cccac433b3eab985968
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-12-28 09:56:08 +0900

    feat: add 메서드가 정수를 0, 1개, 2개 받아 그 합계를 돌려주도록 수정 - 빈 문자열의 경우 0을 반환 - 예를 들어 인자로 "", "1", "1,2" 등이 올 수 있다.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,15 @@
 public class StringCalculator {
 
 	public int add(String numbers) {
-		return -1;
+		if (numbers.equals("")) {
+			return 0;
+		} else {
+			String[] numArr = numbers.split(",");
+			if (numArr.length == 1) {
+				return Integer.parseInt(numArr[0]);
+			} else {
+				return Integer.parseInt(numArr[0]) + Integer.parseInt(numArr[1]);
+			}
+		}
 	}
 }
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,28 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringCalculatorTest {
+	StringCalculator stringCalculator = new StringCalculator();
+
+	@Test
+	void 인자가_빈_문자열이면_0을_반환() {
+		int returnNum = stringCalculator.add("");
+
+		assertThat(returnNum).isEqualTo(0);
+	}
+
+	@Test
+	void 인자가_하나의_정수면_해당_정수를_반환() {
+		int returnNum = stringCalculator.add("1");
+
+		assertThat(returnNum).isEqualTo(1);
+	}
+
+	@Test
+	void 인자가_두개의_정수면_합계를_반환() {
+		int returnNum = stringCalculator.add("1,2");
+
+		assertThat(returnNum).isEqualTo(3);
+	}
+}

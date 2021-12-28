2f0977a102e4e8769e876cbe75d9962daf93dea9
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-12-28 12:00:05 +0900

    feat: add 메서드에 음수를 전달할 경우 "음수는 허용되지 않음: [받은음수]"이라는 메시지가 들어있는 예외를 던지기 - `[받은음수]`에는 문자열에 들어있는 음수가 들어가야 한다. - 음수가 2개 이상 있으면 모든 음수를 메시지에 포함시켜라.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,22 +1,49 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringJoiner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 public class StringCalculator {
 	private final String regExpression = "//(.*?)\n";
 	Pattern speratorPattern = Pattern.compile(regExpression);
 
-	public int add (String numbers) {
+	public int add (String numbers) throws Exception {
 		if (numbers.equals("")) {
 			return 0;
 		} else {
-			String[] numArr = splitStringNumber(numbers);
-			int sum = Arrays.stream(numArr)
+			String[] numStringArr = splitStringNumber(numbers);
+			int[] numArr = Arrays.stream(numStringArr)
 				.mapToInt(stringNum ->  Integer.parseInt(stringNum))
-				.sum();
+				.toArray();
+
+			checkNegative(numArr);
+
+			int sum = Arrays.stream(numArr).sum();
 			return sum;
 		}
 	}
+
+	private void checkNegative(int[] numArr) throws Exception {
+		var negativeList = new ArrayList<Integer>();
+		for (int num : numArr) {
+			if (num < 0) {
+				negativeList.add(num);
+			}
+		}
+
+		if (negativeList.size() >= 1) {
+			StringJoiner sj = new StringJoiner(", ","음수는 허용되지 않음: [","]");
+			for (int negative: negativeList) {
+				sj.add(Integer.toString(negative));
+			}
+
+			throw new Exception(sj.toString());
+		}
+	}
+
+	// 음수는 허용되지 않음: [-1, -4]
 
 	private String[] splitStringNumber (String numbers) {
 		if (hasSeperator(numbers)) {
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -6,38 +6,46 @@ class StringCalculatorTest {
 	StringCalculator stringCalculator = new StringCalculator();
 
 	@Test
-	void 인자가_빈_문자열이면_0을_반환 () {
+	void 인자가_빈_문자열이면_0을_반환 () throws Exception {
 		int returnNum = stringCalculator.add("");
 
 		assertThat(returnNum).isEqualTo(0);
 	}
 
 	@Test
-	void 인자가_하나의_정수면_해당_정수를_반환 () {
+	void 인자가_하나의_정수면_해당_정수를_반환 () throws Exception {
 		int returnNum = stringCalculator.add("1");
 
 		assertThat(returnNum).isEqualTo(1);
 	}
 
 	@Test
-	void 인자가_두개의_정수면_합계를_반환 () {
+	void 인자가_두개의_정수면_합계를_반환 () throws Exception {
 		int returnNum = stringCalculator.add("1,2");
 
 		assertThat(returnNum).isEqualTo(3);
 	}
 
 	@Test
-	void 인자가_두개를_초과하는_정수면_합계를_반환 () {
+	void 인자가_두개를_초과하는_정수면_합계를_반환 () throws Exception {
 		int returnNum = stringCalculator.add("1,2,3,4,5");
 
 		assertThat(returnNum).isEqualTo(15);
 	}
 
 	@Test
-	void 문자열_맨앞에_구분자를_넣으면_해당_구분자로_문자열을_나눈다 () {
+	void 문자열_맨앞에_구분자를_넣으면_해당_구분자로_문자열을_나눈다 () throws Exception {
 		// "//구분자\n숫자"형식으로 구분자를 넣을 수 있음
 		int returnNum = stringCalculator.add("//;\n1;2;3;4;5");
 
 		assertThat(returnNum).isEqualTo(15);
+	}
+
+	@Test
+	void add_메서드에_음수를_전달하면_예외_발생 () {
+		assertThatThrownBy(() -> {
+			int returnNum = stringCalculator.add("//;\n-1;2;3;-4;5");
+		}).isInstanceOf(Exception.class)
+			.hasMessageContaining("음수는 허용되지 않음: [-1, -4]");
 	}
 }

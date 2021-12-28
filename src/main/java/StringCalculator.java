5d7848f741ad0f5f26b508ec0d059e87cf793bf9
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-12-28 11:39:46 +0900

    feat: add 메서드가 다른 구분자를 지원할 수 있게 수정 - 문자열 맨 앞에 구분자를 넣을 수 있어야 한다. - //구분자\n숫자들" 형식으로 구분자가 문자열 앞에 들어갈 수 있다 - 단 구분자는 1글자로만 이뤄진다.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,16 +1,49 @@
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
+	private final String regExpression = "//(.*?)\n";
+	Pattern speratorPattern = Pattern.compile(regExpression);
 
-	public int add(String numbers) {
+	public int add (String numbers) {
 		if (numbers.equals("")) {
 			return 0;
 		} else {
-			String[] numArr = numbers.split(",");
+			String[] numArr = splitStringNumber(numbers);
 			int sum = Arrays.stream(numArr)
-				.mapToInt(stringNum -> Integer.parseInt(stringNum))
+				.mapToInt(stringNum ->  Integer.parseInt(stringNum))
 				.sum();
 			return sum;
+		}
+	}
+
+	private String[] splitStringNumber (String numbers) {
+		if (hasSeperator(numbers)) {
+			String seperator = getSeperator(numbers);
+			numbers = deleteSeperatorPart(numbers);
+
+			return numbers.split(seperator);
+		} else {
+			return numbers.split(",");
+		}
+	}
+
+	private String deleteSeperatorPart(String numbers) {
+		return numbers.replaceAll(regExpression, "");
+	}
+
+	private boolean hasSeperator(String numbers) {
+		Matcher matcher = speratorPattern.matcher(numbers);
+		return matcher.find();
+	}
+
+	private String getSeperator(String numbers) {
+		Matcher matcher = speratorPattern.matcher(numbers);
+		if (matcher.find()) {
+			return matcher.group(1);
+		} else {
+			throw new IllegalStateException("No Match Found");
 		}
 	}
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -6,21 +6,21 @@ class StringCalculatorTest {
 	StringCalculator stringCalculator = new StringCalculator();
 
 	@Test
-	void 인자가_빈_문자열이면_0을_반환() {
+	void 인자가_빈_문자열이면_0을_반환 () {
 		int returnNum = stringCalculator.add("");
 
 		assertThat(returnNum).isEqualTo(0);
 	}
 
 	@Test
-	void 인자가_하나의_정수면_해당_정수를_반환() {
+	void 인자가_하나의_정수면_해당_정수를_반환 () {
 		int returnNum = stringCalculator.add("1");
 
 		assertThat(returnNum).isEqualTo(1);
 	}
 
 	@Test
-	void 인자가_두개의_정수면_합계를_반환() {
+	void 인자가_두개의_정수면_합계를_반환 () {
 		int returnNum = stringCalculator.add("1,2");
 
 		assertThat(returnNum).isEqualTo(3);
@@ -29,6 +29,14 @@ class StringCalculatorTest {
 	@Test
 	void 인자가_두개를_초과하는_정수면_합계를_반환 () {
 		int returnNum = stringCalculator.add("1,2,3,4,5");
+
+		assertThat(returnNum).isEqualTo(15);
+	}
+
+	@Test
+	void 문자열_맨앞에_구분자를_넣으면_해당_구분자로_문자열을_나눈다 () {
+		// "//구분자\n숫자"형식으로 구분자를 넣을 수 있음
+		int returnNum = stringCalculator.add("//;\n1;2;3;4;5");
 
 		assertThat(returnNum).isEqualTo(15);
 	}
